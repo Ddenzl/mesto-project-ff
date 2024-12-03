842f9ad9c3c4a5827486c65bda30ed2a5644b6d3
Ddenzl <kargin.denis@bk.ru>
2024-12-03 21:39:34 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
+
+function createCard(data) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").src = data.link;
+  cardElement.querySelector(".card__image").alt = data.name;
+  cardElement.querySelector(".card__title").textContent = data.name;
+
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  return cardElement;
+}
+
+function renderCards(cards) {
+  cards.forEach(card => {
+      const addCard = createCard(card);
+      placesList.append(addCard);
+  });
+}
+
+renderCards(initialCards);
